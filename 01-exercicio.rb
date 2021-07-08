d28c60f059f09b20eef0b3ad12a6f3bc423c7b03
Nayara Ferraz <ferrazzznay@gmail.com>
2021-07-08 15:24:03 -0300

    Exercicio aula 08-07-21

---
+++ 01-exercicio.rb
@@ -0,0 +1,16 @@
+# exercicio quantas horas há em um ano?
+puts "quantas horas há em um ano? " + (24 * 365).to_s
+
+#quantos minutos há em uma década?
+puts "quantos minutos há em uma década? "  + (365 * 10 * 24 * 60 ).to_s
+
+#qual é a sua idade em segundos?
+puts "qual é a sua idade em segundos? " + (365 * 24 * 60 * 60 * 28).to_s
+
+#quantos chocolates você pretende comer na vida?
+puts "quantos chocolates você pretende comer na vida? " +  (87*12*30).to_s
+
+#Desafio
+#Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
+puts "Desafio"
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos? "  + (1232000000/365/24/60/60).to_s
