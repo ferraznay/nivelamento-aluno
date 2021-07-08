2d5c6aed8abfbdbcaa974e44eaec852473e36dcc
Nayara Ferraz <ferrazzznay@gmail.com>
2021-07-08 17:11:49 -0300

    Exercicio 2 08-07

---
+++ 02-exercicio.rb
@@ -0,0 +1,33 @@
+# Quantas horas há em um ano?
+pergunta1 = "Quantas horas há em um ano? " 
+horasAno = 365 * 24
+
+puts pergunta1 + horasAno.to_s
+
+
+# Quantos minutos há em uma década?
+pergunta2 = "Quantos minutos há em uma década? "
+minutos = 1 * 365 * 10 * 24 * 60 
+
+puts pergunta2 + minutos.to_s
+
+
+# Qual é a sua idade em segundos?
+pergunta3 = "Qual é a sua idade em segundos? "
+idadeSegundos = 365 * 24 * 60 * 60 * 28
+
+puts pergunta3 + idadeSegundos.to_s
+
+# Quantos chocolates você pretende comer na vida?
+pergunta4 = "Quantos chocolates você pretende comer na vida? "
+cholatesVida = 87 * 12 * 30
+
+puts pergunta4 + cholatesVida.to_s
+
+# Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
+desafio = "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos? "
+idadeAnos = 1232000000.0 / 60 / 60 / 24 / 365
+
+puts desafio + idadeAnos.to_s
+
+
